7d319e171c0e3219e28b59908231a203d84ce2b5
forestgood <fgoodiu@gmail.com>
2013-02-09 10:15:21 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twansparent::Application.config.secret_token = '97a1173e3caf1d0b736e3ced7188e5931873221740f30bc112a7ffdcc66f9f3750193019c1b54a001698156b991b85896cc5e428b7d3c278d16f8268d3fb7baf'
