77fba7b722beae60c8348479a848489aa9957233
forestgood <fgoodiu@gmail.com>
2013-02-19 23:29:53 -0500

    Users model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,72 @@
+require 'spec_helper'
+
+describe User do
+  
+  before { @user = User.new(name: "example user", email: "user@example.com", password: "foobar") }
+
+  subject { @user }
+
+  it { should respond_to(:name)  }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+
+  it { should be_valid }
+
+  describe "when name is not present" do
+  	before { @user.name = " " }
+  	it { should_not be_valid }
+  end
+
+  describe "when email is not present" do
+  	before { @user.email = " " }
+  	it { should_not be_valid }
+  end
+
+  describe "when name is too long" do
+  	before { @user.name = "a" * 51 }
+  	it { should_not be_valid }
+  end
+
+  describe "when email format is invalid" do
+  	it "should be invalid" do
+  		addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+  		addresses.each do |invalid_address|
+  			@user.email = invalid_address
+  			@user.should_not be_valid
+		end
+	end
+  end
+
+  describe "when email format is valid" do
+  	it "should be valid" do
+  		addresses = %w[user@foo.com A_USER@f.b.org first.lst@foo.jb a+b@baz.cn]
+  		addresses.each do |valid_addresses|
+  			@user.email = valid_address
+  			@user.should be_valid
+		end
+	end
+  end
+
+  describe "when email address is already taken" do
+  	before do
+  		user_with_same_email = @user.dup
+  		user_with_same_email = @user.email.upcase
+  		user_with_same_email.save
+	end
+
+	it { should_not be_valid }
+  end
+
+  describe "when password is not present" do 
+  	before { @user.password = @user.password_confirmation = " " }
+  	it { should_not be_valid }
+  end
+
+  describe "when password doesn't match confirmation" do
+  	before { @user.password_confirmation = "mismatch"}
+  	it { should_not be_valid }
+  end
+
+
+
+end
