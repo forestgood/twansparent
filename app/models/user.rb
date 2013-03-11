e535634232554e72df73ca79ab7e182c33ab6319
forestgood <fgoodiu@gmail.com>
2013-03-11 13:11:22 -0400

    update User.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_token
   has_secure_password
 
   before_save { |user| user.email = user.email.downcase }
-  before_save create_remember_token
+  before_save :create_remember_token
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
