73c6bacab09c65c0a71e2f80ddcd4363672fe955
forestgood <fgoodiu@gmail.com>
2013-03-11 12:18:07 -0400

    Finsih Sign-in-out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,14 +9,13 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
-	  def current_user=(user)
-	    @current_user = user
-	  end
+	def current_user=(user)
+		@current_user = user
+	end
 
-	  def current_user
-	    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
-	  end
-
+	def current_user
+		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+	end
 
 	def sign_out
 	    self.current_user = nil
